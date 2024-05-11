43afb8a5461fbb87c37da90d3d43175e83fd0688
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-11 13:28:18 +0200

    Removed usings outside of projects

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -3,11 +3,11 @@ global using System;
 global using System.Linq;
 global using System.Text;
 global using System.Collections;
-global using SunamoExceptions.OnlyInSE;
-global using SunamoValues;
-global using SunamoData.Data;
+
+
+
 global using System.Data;
-global using SunamoValues.Constants;
+
 global using SunamoDictionary._sunamo;
 global using System.Collections.Generic;
 global using System.Collections;
