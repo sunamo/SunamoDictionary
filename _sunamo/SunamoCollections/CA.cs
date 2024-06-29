338dc7f5e354eaa9b4c41dc4897f01256a3c4212
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 08:57:40 +0200

    moved all in _sunamo to right subfolders by original project

---
+++ _sunamo/SunamoCollections/CA.cs
@@ -0,0 +1,34 @@
+
+namespace SunamoDictionary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+internal class CA
+{
+    internal static List<string> PostfixIfNotEnding(string pre, List<string> l)
+    {
+        for (int i = 0; i < l.Count; i++)
+        {
+            l[i] = pre + l[i];
+        }
+        return l;
+    }
+
+
+    internal static List<string> Prepend(string v, List<string> toReplace)
+    {
+        for (int i = 0; i < toReplace.Count; i++)
+        {
+            if (!toReplace[i].StartsWith(v))
+            {
+                toReplace[i] = v + toReplace[i];
+            }
+        }
+        return toReplace;
+    }
+}
+
