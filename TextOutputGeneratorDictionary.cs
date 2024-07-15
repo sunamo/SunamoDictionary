656f86057da3c4f07bf4438652faabae7a4ffd4a
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-15 07:08:18 +0200

    internal to public

--- TextOutputGeneratorDictionary.cs
+++ TextOutputGeneratorDictionary.cs
@@ -1,14 +1,13 @@
 namespace SunamoDictionary;
 
-
 public class TextOutputGeneratorDictionary
 {
-    internal void AppendLine(object s)
+    public void AppendLine(object s)
     {
         throw new NotImplementedException();
     }
 
-    internal void Header(string key)
+    public void Header(string key)
     {
         throw new NotImplementedException();
     }
