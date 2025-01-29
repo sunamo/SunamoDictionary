ea2654793cdde37a2a93640c26bc3fcb1c30db58
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-29 22:08:53 +0100

    Remove another unused methods

--- _sunamo/SunamoExceptions/ThrowEx.cs
+++ _sunamo/SunamoExceptions/ThrowEx.cs
@@ -19,8 +19,6 @@ internal partial class ThrowEx
             Exceptions.DifferentCountInLists(FullNameOfExecutedCode(), namefc, countfc, namesc, countsc));
     }
 
-    internal static bool IsNullOrEmpty(string argName, string argValue)
-    { return ThrowIsNotNull(Exceptions.IsNullOrWhitespace(FullNameOfExecutedCode(), argName, argValue, true)); }
 
 
     internal static bool KeyNotFound<T, U>(IDictionary<T, U> en, string dictName, T key)
