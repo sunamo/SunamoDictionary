16d6ccd70adf3a007a3b89bc57f26e958f1cd840
sunamo.cz <radek.jancik@sunamo.cz>
2025-07-30 23:29:17 +0200

    Auto-commit before force push

---
+++ SunamoDictionary.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace SunamoDictionary.Tests;
+
+public class DictionaryHelperTests
+{
+    [Fact]
+    public void CalculateMedianAverageFloatTest()
+    {
+        Dictionary<string, List<float>> v = new Dictionary<string, List<float>>();
+        v.Add("A", new List<float>([1f]));
+
+        //var r = DictionaryHelper.CalculateMedianAverageFloat(v, new TextOut);
+
+    }
+}
